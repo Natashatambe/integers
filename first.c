697df559911ae1f09418f301288374cee285e56b
natasha <108368242+Natashatambe@users.noreply.github.com>
2025-11-26 13:30:25 +0530

    first.c

---
+++ first.c
@@ -0,0 +1,20 @@
+
+
+int main() {
+    int num1, num2, sum;
+
+    // Taking input from user
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Adding two numbers
+    sum = num1 + num2;
+
+    // Displaying the result
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
